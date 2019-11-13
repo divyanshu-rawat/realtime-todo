9c76d290fa2cff5749cbbdf6dce083c6f5d07384
Rawat <divyanshu.r46956@gmail.com>
2019-11-13 14:23:18 +0100

    done with setting up apollo-client.

--- src/components/App.js
+++ src/components/App.js
@@ -5,32 +5,36 @@ import TodoPrivateWrapper from "./Todo/TodoPrivateWrapper";
 import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
 import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import { useAuth0 } from "./Auth/react-auth0-spa";
+
+const createApolloClient = authToken => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: "https://learn.hasura.io/graphql",
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    }),
+    cache: new InMemoryCache()
+  });
+};
 
 const App = ({ idToken }) => {
   const { loading, logout } = useAuth0();
   if (loading) {
     return <div>Loading...</div>;
   }
+
+  const client = createApolloClient(idToken);
   return (
-    <div>
-      <Header logoutHandler={logout} />
-      <div className="row container-fluid p-left-right-0 m-left-right-0">
-        <div className="row col-md-9 p-left-right-0 m-left-right-0">
-          <div className="col-md-6 sliderMenu p-30">
-            <TodoPrivateWrapper />
-          </div>
-          <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
-            <TodoPublicWrapper />
-          </div>
-        </div>
-        <div className="col-md-3 p-left-right-0">
-          <div className="col-md-12 sliderMenu p-30 bg-gray">
-            <OnlineUsersWrapper />
-          </div>
-        </div>
-      </div>
-    </div>
+    <ApolloProvider client={client}>
+      <div />
+    </ApolloProvider>
   );
 };
 
