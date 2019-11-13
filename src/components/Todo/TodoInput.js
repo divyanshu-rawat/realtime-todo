08d7622e5508ca5b6910c7cb2a07bfbea52f63a3
Rawat <divyanshu.r46956@gmail.com>
2019-11-13 16:20:01 +0100

    update client side cache on mutation.

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
+import { GET_MY_TODOS } from "./TodoPrivateList";
+
 const ADD_TODO = gql`
   mutation($todo: String!, $isPublic: Boolean!) {
     insert_todos(objects: { title: $todo, is_public: $isPublic }) {
@@ -18,7 +20,30 @@ const ADD_TODO = gql`
 // eslint-disable-next-line react/prop-types
 const TodoInput = ({ isPublic = false }) => {
   const [todoInput, setTodoInput] = useState("");
-  const [addTodo] = useMutation(ADD_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    update: updateCache,
+    onCompleted: resetInput
+  });
+  //  It receives the result of the mutation (data) and the current cache (store) as arguments.
+  const updateCache = (cache, { data }) => {
+    if (isPublic) {
+      return null;
+    }
+
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS
+    });
+    console.log("data", data);
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({
+      query: GET_MY_TODOS,
+      data: { todos: [newTodo, ...existingTodos.todos] }
+    });
+  };
+
+  const resetInput = () => {
+    setTodoInput("");
+  };
 
   return (
     <form
